36ec643a5d1fc63b900be855f95be61901adf190
Tan NGUYEN <leductan.nguyen@gmail.com>
2019-05-07 22:07:51 +0200

    WIP

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -496,12 +496,20 @@
   #define DEFAULT_X_STEPS_PER_MM 400.0
   #define DEFAULT_Y_STEPS_PER_MM 400.0
   #define DEFAULT_Z_STEPS_PER_MM 400.0
-  #define DEFAULT_X_MAX_RATE 5000.0 // mm/min
-  #define DEFAULT_Y_MAX_RATE 1500.0 // mm/min
-  #define DEFAULT_Z_MAX_RATE 6000.0 // mm/min
-  #define DEFAULT_X_ACCELERATION (1000.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_Y_ACCELERATION (500.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_Z_ACCELERATION (1000.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  // Max rate in comments
+  // #define DEFAULT_X_MAX_RATE 5000.0 // mm/min
+  // #define DEFAULT_Y_MAX_RATE 1500.0 // mm/min
+  // #define DEFAULT_Z_MAX_RATE 6000.0 // mm/min
+  #define DEFAULT_X_MAX_RATE 3000.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 1200.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 3000.0 // mm/min
+  // Max acceleration in comments
+  // #define DEFAULT_X_ACCELERATION (1000.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  // #define DEFAULT_Y_ACCELERATION (500.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  // #define DEFAULT_Z_ACCELERATION (1000.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_X_ACCELERATION (800.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Y_ACCELERATION (400.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (800.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_X_MAX_TRAVEL 1260.0 // mm NOTE: Must be a positive value.
   #define DEFAULT_Y_MAX_TRAVEL 1200.0 // mm NOTE: Must be a positive value.
   #define DEFAULT_Z_MAX_TRAVEL 40.0 // mm NOTE: Must be a positive value.
@@ -520,13 +528,13 @@
   #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
   #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
   #define DEFAULT_INVERT_PROBE_PIN 0 // false
-  #define DEFAULT_LASER_MODE 1 // false
+  #define DEFAULT_LASER_MODE 0 // false
   #define DEFAULT_HOMING_ENABLE 1  // false
-  #define DEFAULT_HOMING_DIR_MASK 3 // move positive dir
-  #define DEFAULT_HOMING_FEED_RATE 2000.0 // mm/min
-  #define DEFAULT_HOMING_SEEK_RATE 2000.0 // mm/min
-  #define DEFAULT_HOMING_DEBOUNCE_DELAY 0 // msec (0-65k)
-  #define DEFAULT_HOMING_PULLOFF -2.0 // mm
+  #define DEFAULT_HOMING_DIR_MASK 2 // move positive dir
+  #define DEFAULT_HOMING_FEED_RATE 500.0 // mm/min, slower speed for bouncing after touching limits
+  #define DEFAULT_HOMING_SEEK_RATE 1500.0 // mm/min, faster speed for searching limits
+  #define DEFAULT_HOMING_DEBOUNCE_DELAY 50 // msec (0-65k)
+  #define DEFAULT_HOMING_PULLOFF -4.0 // mm
 #endif
 
 #endif
